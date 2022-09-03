795f183d3338c8e5c6744da571af2496838a6530
NaN-Simon <goosaimon@gmail.com>
2022-09-03 17:23:15 +0300

    fix: hide&show animantion of datedropdown is correct; change process of creating new object for datedropdown and dropdown

--- src/components/date-dropdown/index.js
+++ src/components/date-dropdown/index.js
@@ -25,24 +25,38 @@ class DateDropdown {
     const { properties } = this.$el.dataset;
     this.properties = JSON.parse(properties);
 
-    this.$start = this.$el.querySelector('.date-dropdown__start .text-field__input');
-    this.datepickerStart = new AirDatepicker(this.$start, {
+    this.setup();
 
+    this.statusDropStart = false;
+    this.statusDropEnd = false;
+  }
+
+  setup() {
+    this.$start = this.$el.querySelector('.date-dropdown__start .text-field__input');
+    this.$arrowStart = this.$start.nextSibling;
+    [this.$arrowStartIcon] = this.$arrowStart.children;
+    this.datepickerStart = new AirDatepicker(this.$start, {
+  
       container: this.$el,
       ...propertiesDefault,
       dateFormat: this.properties.dateFormat,
       startDate: this.properties.startDate,
+      onShow: () => this.$arrowStartIcon.innerHTML = 'expand_less',
+      onHide: () => this.$arrowStartIcon.innerHTML = 'expand_more',
     });
-
+  
     if (this.properties.hasTwoInputs) {
       this.$end = this.$el.querySelector('.date-dropdown__end .text-field__input');
+      this.$arrowEnd = this.$end.nextSibling;
+      [this.$arrowEndIcon] = this.$arrowEnd.children;
       this.datepickerEnd = new AirDatepicker(this.$end, {
-
+  
         container: this.$el,
         ...propertiesDefault,
         dateFormat: this.properties.dateFormat,
         startDate: this.properties.endDate,
-
+        onShow: () => this.$arrowEndIcon.innerHTML = 'expand_less',
+        onHide: () => this.$arrowEndIcon.innerHTML = 'expand_more',
       });
     }
   }
@@ -52,3 +66,6 @@ const datedropdown = document.querySelectorAll('.indexjs-date-dropdown');
 datedropdown.forEach((elem) => {
   new DateDropdown(`#${elem.id}`);
 });
+
+// const datedropdown = new DateDropdown(`#text-field-datepicker`)
+// window.datedropdown = datedropdown
