7e9bc4ca8a8001de5b7c311ab041d3f5ca606325
NaN-Simon <goosaimon@gmail.com>
2022-10-18 19:10:53 +0300

    fix: add gradient chart, pixel-perfect room details page

--- src/components/chart/index.js
+++ src/components/chart/index.js
@@ -13,12 +13,10 @@ class ChartDoughnut {
 
   setup() {
     const data = [];
-    const bg = [];
     const bgBorder = [];
 
     this.properties.itemList.forEach((el) => {
       data.push(el.data);
-      bg.push(el.bg);
       bgBorder.push(el.bgBorder);
     });
 
@@ -26,9 +24,14 @@ class ChartDoughnut {
       type: 'doughnut',
       data: {
         datasets: [{
-          label: '# of Votes',
           data,
-          backgroundColor: bg,
+          backgroundColor: (context) => {
+            const bgGradient = [];
+            this.properties.itemList.forEach((el) => {
+              bgGradient.push(getGradient(context, el.bg[0], el.bg[1]));
+            });
+            return bgGradient;
+          },
           borderColor: bgBorder,
           borderWidth: 2,
           cutout: this.properties.cutout,
@@ -46,5 +49,28 @@ class ChartDoughnut {
   }
 }
 
+function getGradient(context, color1, color2) {
+  const { chart } = context;
+  const { ctx, chartArea } = chart;
+
+  if (!chartArea) {
+    return null;
+  }
+
+  if (color2 === undefined) {
+    color2 = color1;
+  }
+
+  const gradientBg = ctx.createLinearGradient(
+    chartArea.left,
+    chartArea.top,
+    chartArea.right,
+    chartArea.bottom,
+  );
+  gradientBg.addColorStop(0, color1);
+  gradientBg.addColorStop(1, color2);
+  return gradientBg;
+}
+
 const chart = document.querySelectorAll('.chart-js');
-chart.forEach((selector) => { new ChartDoughnut(selector) });
+chart.forEach((selector) => { new ChartDoughnut(selector); });
